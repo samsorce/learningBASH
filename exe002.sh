2b15dd9041c8f6c819f5d73033599e73932625d4
samsorce <samcodebeliver@gmail.com>
2024-05-12 21:16:14 -0300

    Create exe002.sh

---
+++ exe002.sh
@@ -0,0 +1,8 @@
+# Crie um script que leia o dia, o mes, e o ano de nascimento de uma pessoa
+# mostre uma mensagem com a data formatada
+
+read -p "Dia: " dia
+read -p "Mes: " mes
+read -p "Ano: " ano
+
+echo "Você nasceu no dia ${dia} do mes ${mes} do ano  ${ano}."
