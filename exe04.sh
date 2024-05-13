8666ce35d06b582db14457a2029d2ff394926204
samsorce <samcodebeliver@gmail.com>
2024-05-13 09:24:10 -0300

    Update exe04.sh

--- exe04.sh
+++ exe04.sh
@@ -3,34 +3,19 @@
 
 read -p "Digite algo: " dado
 
+
 isalpha='^[a-zA-Z0-9]*$'
 isdecimal='^[0-9]*\.[0-9]*$'
 isdigit='^[0-9]*$'
 isidentifier='^(%|builtin|dirs|for|local|select|type|\(\(|caller|disown|for|logout|set|typeset|\.|case|echo|function|mapfile|shift|ulimit|:|cd|enable|getopts|popd|shopt|umask|\[|command|eval|hash|printf|source|unalias|\[\[|compgen|exec|help|pushd|suspend|unset|\{||complete|exit|history|pwd|test|until|alias|compopt|export|if|read|time|variables|bg|continue|false|jobs|readarray|times|wait|bind|coproc|fc|kill|readonly|trap|while|break|declare|fg|let|return|true)$'
+islower='^[a-z]*$'
+isupper='^[A-Z]*$'
+istitle='^[A-Z][a-z]*$'
 
-if [[ ${dado} =~ ${isalpha} ]]; then
-	echo "é alphanumerico : TRUE"
-else
-	echo "é alphanumerico : FALSE"
-fi
-
-
-if [[ ${dado} =~ ${isdecimal} ]]; then
-	echo "é decimal       : TRUE"
-else
-	echo "é decimal       : FALSE"
-fi
-
-
-if [[ ${dado} =~ ${isdigit} ]]; then
-	echo "é digito        : TRUE"
-else
-	echo "é digito        : FALSE"
-fi
-
-
-if [[ ${dado} =~ ${isidentifier} ]]; then
-	echo "é indentificador: TRUE"
-else
-	echo "é indentificador: FALSE"
-fi
+echo -n "é alphanumerico : "; [[ ${dado} =~ ${isalpha} ]]      && echo "TRUE" || echo "FALSE"
+echo -n "é decima        : "; [[ ${dado} =~ ${isdecimal} ]]    && echo "TRUE" || echo "FALSE"
+echo -n "é digito        : "; [[ ${dado} =~ ${isdigit} ]]      && echo "TRUE" || echo "FALSE"
+echo -n "é indentificador: "; [[ ${dado} =~ ${isidentifier} ]] && echo "TRUE" || echo "FALSE"
+echo -n "é lowercase     : "; [[ ${dado} =~ ${islower} ]]      && echo "TRUE" || echo "FALSE"
+echo -n "é uppercase     : "; [[ ${dado} =~ ${isupper} ]]      && echo "TRUE" || echo "FALSE"
+echo -n "é title         : "; [[ ${dado} =~ ${istitle} ]]      && echo "TRUE" || echo "FALSE"
