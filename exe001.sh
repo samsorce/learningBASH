39b18d9010fdb82c11ddf6f63340e3a5e9126a88
samsorce <samcodebeliver@gmail.com>
2024-05-12 21:13:16 -0300

    Create exe001.sh

---
+++ exe001.sh
@@ -0,0 +1,5 @@
+# Crie um script que leia o nome de uma pessoa e mostre uma mensagem de
+# boas vindas de acordo com o valor digitado
+
+read -p "Ola, qual é o seu nome? " nome
+echo "Muito bem! Seja bem vindo(a) ${nome}"
