33b9ba0bb207c2ba72af810963ba2b4b28822599
samsorce <samcodebeliver@gmail.com>
2024-05-15 20:54:46 -0300

    Create exe009.sh

---
+++ exe009.sh
@@ -0,0 +1,15 @@
+# Ler um número qualquer e mostre na tela a sua tabuada
+
+
+read -p "Enter a number: " number
+
+echo "${number} x 1  = $(( number*1 ))"
+echo "${number} x 2  = $(( number*2 ))"
+echo "${number} x 3  = $(( number*3 ))"
+echo "${number} x 4  = $(( number*4 ))"
+echo "${number} x 5  = $(( number*5 ))"
+echo "${number} x 6  = $(( number*6 ))"
+echo "${number} x 7  = $(( number*7 ))"
+echo "${number} x 8  = $(( number*8 ))"
+echo "${number} x 9  = $(( number*9 ))"
+echo "${number} x 10 = $(( number*10 ))"
